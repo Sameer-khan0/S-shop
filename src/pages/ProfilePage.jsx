2cb4639484bcd435ff912053faa08bbe96e54bb5
Muhammad Sameer <136972597+Sameer-khan0@users.noreply.github.com>
2024-03-07 18:49:36 +0500

    react

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,15 +1,14 @@
 import { motion } from "framer-motion";
 import Navbar from "../components/Navbar";
-import Developer from "../assets/user.jpg";
+import Avatar from "../assets/user.jpg";
 import wave from "../assets/wave.png";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUser } from "../redux/user/userFunction";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
-import Context from "../context/Context"
+import Context from "../context/Context";
 
 const DeveloperContactPage = () => {
-
   const context = useContext(Context);
   const { showalert } = context;
   const [imageSelected, setImageSelected] = useState();
@@ -25,17 +24,31 @@ const DeveloperContactPage = () => {
 
   const handelUpdate = async () => {
     try {
-      const url="https://ecommerce-app-ashen-eight.vercel.app/api/user/profile/edit"
-      const req = await axios.put(url,file)
-      if (req.data.status === "ok") {
-        showalert("Updated", true, true);
-      } else {
-        showalert("Something want wrong", false, false);
+      document.getElementById("updateuser").innerHTML = "updating...";
+      
+      const url = "https://ecommerce-app-ashen-eight.vercel.app/api/user/profile/edit";
+      const formDataToSend = new FormData();
+      formDataToSend.append("file", file); 
+
+      const req = await axios.put(url, formDataToSend, {
+        headers: {
+          token: localStorage.getItem('token'),
+         }
+        });
+        
+        if (req.data.status === "ok") {
+          showalert("Updated", true, true);
+        } else {
+          showalert("Something went wrong", false, false);
+        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
-    }
+      finally{
+        document.getElementById("updateuser").innerHTML = "update";
+      }
   };
+  
 
   useEffect(() => {
     dispatch(fetchUser());
@@ -65,7 +78,7 @@ const DeveloperContactPage = () => {
                 <motion.img
                   whileHover={{ scale: 1.1 }}
                   className="rounded-full object-cover w-full h-full"
-                  src={imageSelected || Developer}
+                  src={imageSelected ? imageSelected : (user.img ? user.img : Avatar)}
                   alt="Profile"
                 />
                 <input
@@ -85,10 +98,15 @@ const DeveloperContactPage = () => {
                   <p className="text-xl font-semibold">{user.username}</p>
                   <p className="text-green-500 font-semibold">Customer</p>
                   <p className="text-gray-700">{user.email}</p>
-                  { imageSelected && <button onClick={handelUpdate} className="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded">
-                    Update
-                  </button>
-                  }
+                  {imageSelected && (
+                    <button
+                    id="updateuser"
+                      onClick={handelUpdate}
+                      className="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded"
+                    >
+                      Update
+                    </button>
+                  )}
                 </>
               )}
             </div>
